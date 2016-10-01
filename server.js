2e3872c6e0b113499d40595ed8621ebc56cdb7c2
Oskar Konstantyner <oskar@konstanto.dk>
2016-10-01 10:49:35 +0200

    Implement authentication for messenger bot

--- server.js
+++ server.js
@@ -15,7 +15,18 @@ router.get('/', function(req, res) {
     res.json({ message: 'hooray! welcome to our api!' });   
 });
 
-app.use('/api', router);
-console.log(port);
+const VALIDATION_TOKEN = "konstanto_is_testing_messenger_bots_because_it_is_cool";
+
+router.get('/webhook', function(req, res) {
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === VALIDATION_TOKEN) {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);          
+  }  
+});
+
+app.use('/', router);
 app.listen(port);
-console.log('Magic happens on port ' + port);
